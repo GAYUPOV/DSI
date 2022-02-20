cf7870896d5ec8ca443cad3469d67af7f939a933
beka <bgayupov02@gmail.com>
2022-02-20 19:10:22 +0300

    prosta

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function navburger() {
+    var navmobile = document.getElementById("navburger");
+    navmobile.classList.toggle('nav-active');
+    var hamburger = document.getElementById("hamburger");
+    hamburger.classList.toggle('hamburger-active');
+  }
